0c7c4e54d5190cc9dfceaa564635fac69e032253
KeerthanaAppxperts <keerthana.appxperts@gmail.com>
2026-04-20 19:48:04 +0530

    Add files via upload

---
+++ services-1/Section1.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+// Services 1 Section 1 - Hero (Orisa Studio + contact + banner)
+
+export default function Section1() {
+    return (
+      <section className="sec-1-services pt-150 border-bottom-100 overflow-hidden">
+        <div className="container">
+          <div className="row align-items-center mb-20">
+            <div className="col-lg-9">
+              <h1 className="section-title d-flex fw-600 fz-200 reveal-text mb-0">
+                Risper CRM<sup>®</sup>
+              </h1>
+            </div>
+            <div className="col-lg-3 ms-auto text-lg-end">
+              <h5>
+                <a
+                  href="mailto:info@rispercrm.com"
+                  className="text-decoration-none"
+                >
+                  info@rispercrm.com
+                </a>
+              </h5>
+              <h6 className="fw-600">
+                <a href="tel:+971585901002" className="text-decoration-none">
+                  +971585901002
+                </a>
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div className="at-banner-thumb overflow-hidden scale-up-img">
+          <Image
+            className="img-cover scale-up"
+            data-speed=".4"
+            src="/assets/imgs/pages/img-153.webp"
+            alt="orisa"
+            width={1920}
+            height={800}
+          />
+        </div>
+      </section>
+    );
+}
